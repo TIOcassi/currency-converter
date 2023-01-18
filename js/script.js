a37f2fdc571ad862b11b331ac24d8e8f4cb51eba
Krystian Zajac <tiocassi1@wp.pl>
2023-01-18 19:24:22 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let formElement = document.querySelector(".js-form");
+let currencyElement = document.querySelector(".js-currency");
+let amountElement = document.querySelector(".js-amount");
+let resultElement = document.querySelector(".js-result");
+
+let rateEUR = 4.65;
+let rateUSD = 4.38;
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+
+    let result;
+
+    switch (currency) {
+        case "EUR":
+            result = amount / rateEUR;
+            break;
+        case "USD":
+            result = amount / rateUSD;
+            break;
+    }
+
+    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+});
